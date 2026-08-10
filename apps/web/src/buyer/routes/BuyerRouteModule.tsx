5d54e8951d2126154d82f143984624f009a8a8be
yueguangbailiangpin <4vyvj8jycy@privaterelay.appleid.com>
2026-08-10 23:28:55 +0800

    feat: route frozen buyer products and tasks

--- apps/web/src/buyer/routes/BuyerRouteModule.tsx
+++ apps/web/src/buyer/routes/BuyerRouteModule.tsx
@@ -1,14 +1,16 @@
-import { useLocation } from 'react-router';
+import { Navigate, useLocation } from 'react-router';
 import { BuyerLayout } from './BuyerLayout';
 import { BuyerRouteProvider } from '../../routes/IdentityRouteSlots';
 import { RouteChunkBoundary } from '../../routes/RouteChunkBoundary';
-import { BuyerDashboardPage } from '../dashboard/BuyerDashboardPage';
-import { BuyerDemandDetailPage } from '../demands/BuyerDemandDetailPage'; import { BuyerDemandsPage } from '../demands/BuyerDemandsPage';
-import { BuyerReservationDetailPage } from '../reservations/BuyerReservationDetailPage'; import { BuyerReservationsPage } from '../reservations/BuyerReservationsPage';
+import { BuyerDemandDetailPage } from '../demands/BuyerDemandDetailPage';
+import { BuyerDemandsPage } from '../demands/BuyerDemandsPage';
+import { BuyerReservationDetailPage } from '../reservations/BuyerReservationDetailPage';
+import { BuyerReservationsPage } from '../reservations/BuyerReservationsPage';
 
 const loadBuyerInstructionRoute = () => import('./BuyerInstructionRouteModule');
 const loadBuyerOrderRoutes = () => import('./BuyerOrderRouteModule');
 const loadBuyerAfterSalesRoutes = () => import('./BuyerAfterSalesRouteModule');
+const loadBuyerTasksRoute = () => import('./BuyerTasksRouteModule');
 
 export { BuyerLayout };
 
@@ -18,12 +20,14 @@ export default function BuyerPortal(): React.JSX.Element {
 
 export function BuyerRoutePage(): React.JSX.Element {
   const { pathname } = useLocation();
+  if (pathname === '/buyer') return <Navigate to="/buyer/products" replace />;
+  if (pathname === '/buyer/products' || pathname === '/buyer/demands') return <BuyerDemandsPage />;
+  if (pathname === '/buyer/tasks') return <RouteChunkBoundary load={loadBuyerTasksRoute} />;
   if (/^\/buyer\/demands\/[^/]+$/u.test(pathname)) return <BuyerDemandDetailPage />;
-  if (pathname === '/buyer/demands') return <BuyerDemandsPage />;
   if (/^\/buyer\/reservations\/[^/]+\/instruction$/u.test(pathname)) return <RouteChunkBoundary load={loadBuyerInstructionRoute} />;
   if (/^\/buyer\/reservations\/[^/]+$/u.test(pathname)) return <BuyerReservationDetailPage />;
   if (pathname === '/buyer/reservations') return <BuyerReservationsPage />;
   if (pathname.startsWith('/buyer/order-materials') || pathname.startsWith('/buyer/orders') || pathname === '/buyer/me') return <RouteChunkBoundary load={loadBuyerOrderRoutes} />;
   if (pathname.startsWith('/buyer/reviews') || pathname.startsWith('/buyer/refunds')) return <RouteChunkBoundary load={loadBuyerAfterSalesRoutes} />;
-  return <BuyerDashboardPage />;
+  return <Navigate to="/buyer/products" replace />;
 }
