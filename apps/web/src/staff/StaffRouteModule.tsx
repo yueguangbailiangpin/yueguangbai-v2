3b30f25b554e1ddaf1601b143bcbb73f4b07fdb9
yueguangbailiangpin <4vyvj8jycy@privaterelay.appleid.com>
2026-08-11 10:33:14 +0800

    feat: use integrity-aware frozen staff workbench

--- apps/web/src/staff/StaffRouteModule.tsx
+++ apps/web/src/staff/StaffRouteModule.tsx
@@ -2,7 +2,7 @@ import { Navigate, useLocation } from 'react-router';
 import { StaffShell } from './StaffShell';
 import { StaffRouteProvider } from '../routes/IdentityRouteSlots';
 import { RouteChunkBoundary } from '../routes/RouteChunkBoundary';
-import { FrozenStaffWorkbench } from './FrozenStaffWorkbench';
+import { FrozenStaffWorkbenchV2 } from './FrozenStaffWorkbenchV2';
 import { AcquisitionCoreWorkbench } from './acquisition/AcquisitionCoreWorkbench';
 import { BuyerCustomersWorkspace, SellerCustomersWorkspace } from './acquisition/CustomerIntakeWorkspace';
 import { FrozenSellerPrincipalRatePolicyWorkspace } from './pricing/FrozenSellerPrincipalRatePolicyWorkspace';
@@ -28,5 +28,5 @@ export function StaffRoutePage():React.JSX.Element{
   if(pathname.startsWith('/staff/access-management'))return <RouteChunkBoundary load={loadStaffAccessManagementRoutes}/>;
   if(pathname.startsWith('/staff/products')||/^\/staff\/demands\/[^/]+\/reservations$/u.test(pathname))return <RouteChunkBoundary load={loadStaffSchedulingRoutes}/>;
   if(pathname.startsWith('/staff/seller-principal-rate-policies'))return <FrozenSellerPrincipalRatePolicyWorkspace/>;
-  return <FrozenStaffWorkbench/>;
+  return <FrozenStaffWorkbenchV2/>;
 }
