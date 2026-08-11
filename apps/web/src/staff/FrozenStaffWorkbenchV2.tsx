b5dadc236770fd6e29e44c86146e38d943c99dcc
yueguangbailiangpin <4vyvj8jycy@privaterelay.appleid.com>
2026-08-11 10:32:53 +0800

    feat: attach integrity tools to frozen staff workbench

---
+++ apps/web/src/staff/FrozenStaffWorkbenchV2.tsx
@@ -0,0 +1,6 @@
+import { FrozenStaffWorkbench } from './FrozenStaffWorkbench';
+import { StaffOperatingIntegrityTools } from './StaffOperatingIntegrityTools';
+
+export function FrozenStaffWorkbenchV2():React.JSX.Element{
+  return <><FrozenStaffWorkbench/><StaffOperatingIntegrityTools/></>;
+}
